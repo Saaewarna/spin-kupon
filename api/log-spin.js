36f3726d47763a6f4f9d36e3c6c74187bfcb7f78
Saaewarna <saaewarna@gmail.com>
2025-04-07 05:37:24 +0700

    fix: switch to env credentials for Vercel

--- api/log-spin.js
+++ api/log-spin.js
@@ -1,7 +1,7 @@
 import { google } from 'googleapis';
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: 'credentials.json',
+  credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
   scopes: ['https://www.googleapis.com/auth/spreadsheets']
 });
 
